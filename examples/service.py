6e99162c3a69795cce5badad5f3c4481e14454ff
Max <bigin.maks@gmail.com>
2022-07-23 15:54:27 +0300

    more service methods

--- examples/service.py
+++ examples/service.py
@@ -1,7 +1,11 @@
+import uuid
+from datetime import datetime
+from typing import Sequence
+
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from examples.dto import ApplicationCreate
+from examples.dto import ApplicationCreate, ApplicationFilter
 from examples.entity import Application
 from examples.mocks import get_session
 from pytorm.repository import InjectRepository
@@ -16,4 +20,18 @@ class ApplicationService(object):
 
     async def create(self, dto: ApplicationCreate) -> Application:
         application = self.repository.create(**dto.dict())
+        return await self.repository.save(application)
+
+    async def find(self, dto: ApplicationFilter) -> Sequence[Application]:
+        return await self.repository.find(
+            Application.phone.contains(dto.phone),
+            Application.email.contains(dto.email),
+        )
+
+    async def find_one_or_fail(self, application_id: uuid.UUID) -> Application:
+        return await self.repository.find_one_or_fail(id=application_id)
+
+    async def delete(self, application_id: uuid.UUID) -> Application:
+        application = await self.find_one_or_fail(application_id)
+        self.repository.merge(application, deleted_at=datetime.now())
         return await self.repository.save(application)
