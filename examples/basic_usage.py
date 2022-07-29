51fcc7dd6143918d2b3ec3df6aaef607c76221e1
Max <bigin.maks@gmail.com>
2022-07-29 23:48:54 +0300

    basic usage

---
+++ examples/basic_usage.py
@@ -0,0 +1,63 @@
+import asyncio
+from typing import Sequence
+from uuid import UUID
+
+from examples.service import ApplicationService
+from examples.usecase.database import context_session
+from examples.usecase.dto import (
+    ApplicationCreate,
+    ApplicationFilter,
+    ApplicationRead,
+)
+
+
+async def read_applications(
+    application_service: ApplicationService,
+) -> Sequence[ApplicationRead]:
+    dto = ApplicationFilter(
+        phone='+78005553535',
+        email='user@example.com',
+    )
+    applications = await application_service.find(dto)
+    return [
+        ApplicationRead.from_orm(application) for application in applications
+    ]
+
+
+async def read_application(
+    application_service: ApplicationService, application_id: UUID,
+) -> ApplicationRead:
+    application = await application_service.find_one_or_fail(application_id)
+    return ApplicationRead.from_orm(application)
+
+
+async def create_application(
+    application_service: ApplicationService,
+) -> ApplicationRead:
+    dto = ApplicationCreate(
+        phone='+78005553535',
+        email='user@example.com',
+        text='Your service is excellent!',
+    )
+    application = await application_service.create(dto)
+    return ApplicationRead.from_orm(application)
+
+
+async def delete_application(
+    application_service: ApplicationService, application_id: UUID,
+) -> ApplicationRead:
+    application = await application_service.delete(application_id)
+    return ApplicationRead.from_orm(application)
+
+
+async def main() -> None:
+    async with context_session() as session:
+        application_service = ApplicationService(session)
+        application = await create_application(application_service)
+        await read_application(application_service, application.id)
+        await delete_application(application_service, application.id)
+        await read_applications(application_service)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
