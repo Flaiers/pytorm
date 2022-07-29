63087f69d22daf0f24d50b1c980f4ec41f9754a2
Max <bigin.maks@gmail.com>
2022-07-29 22:33:06 +0300

    classmethod

--- pytorm/repository/repository.py
+++ pytorm/repository/repository.py
@@ -5,6 +5,7 @@ from multimethod import multimethod as overload
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import Query
 
 from pytorm.repository import AbstractRepository
 
@@ -14,115 +15,129 @@ Model = TypeVar('Model', bound=Base)
 
 class Repository(AbstractRepository, Generic[Model]):
 
-    def __init__(self, session: AsyncSession) -> None:
-        self.session = session
-        self.primary_keys = (
-            pk
-            for pk in self.model.__mapper__.primary_key
-            if pk.server_default is not None
-        )
+    @classmethod
+    def query(cls, *entities) -> Query:
+        return cls.query_cls(entities, cls.session)
 
-    def create(self, **attrs) -> Model:
-        return self.model(**attrs)
+    @classmethod
+    def create(cls, **attrs) -> Model:
+        return cls.model_cls(**attrs)
 
-    def merge(self, instance: Model, **attrs) -> Model:
+    @classmethod
+    def merge(cls, instance: Model, **attrs) -> Model:
         for attr_key, attr_value in attrs.items():
             setattr(instance, attr_key, attr_value)
 
         return instance
 
-    def has_pk(self, instance: Model) -> bool:
-        return bool([
-            attr
-            for pk in self.primary_keys
-            if (attr := getattr(instance, pk.name)) is not None
-        ])
+    @classmethod
+    def has_pk(cls, instance: Model) -> bool:
+        return bool(cls.get_pk(instance))
 
-    def get_pk(self, instance: Model) -> Dict[str, Any] | Any:
-        primary_keys = {
+    @classmethod
+    def get_pk(cls, instance: Model) -> Dict[str, Any] | Any:
+        server_default_pks = (
+            pk
+            for pk in cls.model_cls.__mapper__.primary_key
+            if pk.server_default is not None
+        )
+        pks = {
             pk.name: attr
-            for pk in self.primary_keys
+            for pk in server_default_pks
             if (attr := getattr(instance, pk.name)) is not None
         }
 
-        if len(primary_keys) > 1:
-            return primary_keys
+        if len(pks) > 1:
+            return pks
 
-        return next(iter(primary_keys.values()))
+        return next(iter(pks.values()))
 
-    async def count(self, *where, **attrs) -> int:
+    @classmethod
+    async def count(cls, *where, **attrs) -> int:
         statement = sa.select(sa.func.count(
-        )).select_from(self.model).where(*where).filter_by(**attrs)
-        return await self.session.scalar(statement)
+        )).select_from(cls.model_cls).where(*where).filter_by(**attrs)
+        return await cls.session.scalar(statement)
 
-    async def update(self, *where, values: Dict[str, Any], **attrs) -> None:
-        statement = sa.update(self.model).where(*where).filter_by(**attrs).values(**values)
-        await self.session.execute(statement)
-        await self.session.commit()
+    @classmethod
+    async def update(cls, *where, values: Dict[str, Any], **attrs) -> None:
+        statement = sa.update(cls.model_cls).where(*where).filter_by(**attrs).values(**values)
+        await cls.session.execute(statement)
+        await cls.session.commit()
 
-    async def delete(self, *where, **attrs) -> None:
-        statement = sa.delete(self.model).where(*where).filter_by(**attrs)
-        await self.session.execute(statement)
-        await self.session.commit()
+    @classmethod
+    async def delete(cls, *where, **attrs) -> None:
+        statement = sa.delete(cls.model_cls).where(*where).filter_by(**attrs)
+        await cls.session.execute(statement)
+        await cls.session.commit()
 
-    async def find(self, *where, **attrs) -> Sequence[Model]:
-        statement = sa.select(self.model).where(*where).filter_by(**attrs)
-        return (await self.session.scalars(statement)).unique().all()
+    @classmethod
+    async def find(cls, *where, **attrs) -> Sequence[Model]:
+        statement = sa.select(cls.model_cls).where(*where).filter_by(**attrs)
+        return (await cls.session.scalars(statement)).unique().all()
 
-    async def find_one(self, *where, **attrs) -> Model | None:
-        statement = sa.select(self.model).where(*where).filter_by(**attrs)
-        return await self.session.scalar(statement)
+    @classmethod
+    async def find_one(cls, *where, **attrs) -> Model | None:
+        statement = sa.select(cls.model_cls).where(*where).filter_by(**attrs)
+        return await cls.session.scalar(statement)
 
-    async def find_one_or_fail(self, *where, **attrs) -> Model:
-        instance = await self.find_one(*where, **attrs)
+    @classmethod
+    async def find_one_or_fail(cls, *where, **attrs) -> Model:
+        instance = await cls.find_one(*where, **attrs)
         if instance is None:
-            raise NoResultFound('{0.__name__} not found'.format(self.model))
+            raise NoResultFound('{0.__name__} not found'.format(cls.model_cls))
 
         return instance
 
     @overload
-    async def remove(self, instance: Model) -> None:
-        await self.session.delete(instance)
-        await self.session.commit()
+    @classmethod
+    async def remove(cls, instance: Model) -> None:
+        await cls.session.delete(instance)
+        await cls.session.commit()
 
     @overload
-    async def remove(self, instances: Sequence[Model]) -> None:
+    @classmethod
+    async def remove(cls, instances: Sequence[Model]) -> None:
         for instance in instances:
-            await self.session.delete(instance)
+            await cls.session.delete(instance)
 
-        await self.session.commit()
+        await cls.session.commit()
 
     @overload
-    async def pre_save(self, instance: Model) -> Model:
-        if self.has_pk(instance):
-            return await self.session.merge(instance)
+    @classmethod
+    async def pre_save(cls, instance: Model) -> Model:
+        if cls.has_pk(instance):
+            return await cls.session.merge(instance)
 
-        self.session.add(instance)
-        await self.session.flush()
+        cls.session.add(instance)
+        await cls.session.flush()
         return instance
 
     @overload
-    async def pre_save(self, instances: Sequence[Model]) -> Sequence[Model]:
-        self.session.add_all(instances)
-        await self.session.flush()
+    @classmethod
+    async def pre_save(cls, instances: Sequence[Model]) -> Sequence[Model]:
+        cls.session.add_all(instances)
+        await cls.session.flush()
         return instances
 
     @overload
-    async def save(self, instance: Model) -> Model:
-        instance = await self.pre_save(instance)
-        await self.session.commit()
+    @classmethod
+    async def save(cls, instance: Model) -> Model:
+        instance = await cls.pre_save(instance)
+        await cls.session.commit()
         return instance
 
     @overload
-    async def save(self, instances: Sequence[Model]) -> Sequence[Model]:
-        instances = await self.pre_save(instances)
-        await self.session.commit()
+    @classmethod
+    async def save(cls, instances: Sequence[Model]) -> Sequence[Model]:
+        instances = await cls.pre_save(instances)
+        await cls.session.commit()
         return instances
 
 
-def InjectRepository(model: Model, session: AsyncSession) -> Repository[Model]:
-    class_name = '{0.__name__}{1.__name__}'.format(model, Repository)
+def InjectRepository(
+    model_cls: Type[Model], session: AsyncSession, query_cls: Type[Query] = Query,
+) -> Type[Repository[Model]]:
+    class_name = '{0.__name__}{1.__name__}'.format(model_cls, Repository)
     class_bases = (Repository,)
-    class_namespace = {'model': model}
-    repository = type(class_name, class_bases, class_namespace)
-    return repository(session=session)
+    class_namespace = {'session': session, 'model_cls': model_cls, 'query_cls': query_cls}
+    return type(class_name, class_bases, class_namespace)
