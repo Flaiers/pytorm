9b9db26effd3fd5b3cca09b178f29f1e811800eb
Max <bigin.maks@gmail.com>
2022-07-29 23:48:43 +0300

    usecase

--- examples/usecase/dto.py
+++ examples/usecase/dto.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
 
 from pydantic import BaseModel, EmailStr, Field
 
@@ -8,6 +10,22 @@ class ApplicationCreate(BaseModel):
     phone: str = Field(..., regex=r'^(\+)[1-9][0-9\-().]{9,15}$')
     email: EmailStr
     text: str
+
+
+class ApplicationRead(BaseModel):
+
+    id: UUID
+
+    phone: str
+    email: EmailStr
+    text: str
+
+    created_at: datetime
+    updated_at: datetime | None
+    deleted_at: datetime | None
+
+    class Config(object):
+        orm_mode = True
 
 
 @dataclass
