a078bd29bc18cc52e444d5e51c587cb4af9a40a2
Max <bigin.maks@gmail.com>
2022-08-06 01:12:29 +0300

    typer

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,8 @@
 import asyncio
-from typing import Sequence
+from functools import wraps
 from uuid import UUID
+
+import typer
 
 from examples.service import ApplicationService
 from examples.usecase.database import context_session
@@ -11,53 +13,66 @@ from examples.usecase.dto import (
 )
 
 
-async def read_applications(
-    application_service: ApplicationService,
-) -> Sequence[ApplicationRead]:
+class Typer(typer.Typer):
+
+    def async_command(self, *args, **kwargs):
+        def decorator(async_func):
+            @wraps(async_func)
+            def sync_func(*_args, **_kwargs):  # noqa: WPS430
+                return asyncio.run(async_func(*_args, **_kwargs))
+
+            self.command(*args, **kwargs)(sync_func)
+            return async_func
+
+        return decorator
+
+
+app = Typer()
+
+
+@app.async_command()
+async def read_applications() -> None:
     dto = ApplicationFilter(
         phone='+78005553535',
         email='user@example.com',
     )
-    applications = await application_service.find(dto)
-    return [
-        ApplicationRead.from_orm(application) for application in applications
-    ]
+    async with context_session() as session:
+        application_service = ApplicationService(session)
+        applications = await application_service.find(dto)
+        typer.echo([
+            ApplicationRead.from_orm(application)
+            for application in applications
+        ])
 
 
-async def read_application(
-    application_service: ApplicationService, application_id: UUID,
-) -> ApplicationRead:
-    application = await application_service.find_one_or_fail(application_id)
-    return ApplicationRead.from_orm(application)
+@app.async_command()
+async def read_application(application_id: UUID) -> None:
+    async with context_session() as session:
+        application_service = ApplicationService(session)
+        application = await application_service.find_one_or_fail(application_id)
+        typer.echo(ApplicationRead.from_orm(application))
 
 
-async def create_application(
-    application_service: ApplicationService,
-) -> ApplicationRead:
+@app.async_command()
+async def create_application() -> None:
     dto = ApplicationCreate(
         phone='+78005553535',
         email='user@example.com',
         text='Your service is excellent!',
     )
-    application = await application_service.create(dto)
-    return ApplicationRead.from_orm(application)
-
-
-async def delete_application(
-    application_service: ApplicationService, application_id: UUID,
-) -> ApplicationRead:
-    application = await application_service.delete(application_id)
-    return ApplicationRead.from_orm(application)
-
-
-async def main() -> None:
     async with context_session() as session:
         application_service = ApplicationService(session)
-        application = await create_application(application_service)
-        await read_application(application_service, application.id)
-        await delete_application(application_service, application.id)
-        await read_applications(application_service)
+        application = await application_service.create(dto)
+        typer.echo(ApplicationRead.from_orm(application))
+
+
+@app.async_command()
+async def delete_application(application_id: UUID) -> None:
+    async with context_session() as session:
+        application_service = ApplicationService(session)
+        application = await application_service.delete(application_id)
+        typer.echo(ApplicationRead.from_orm(application))
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    app()
